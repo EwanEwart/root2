be950a7c5e64e509fa8d04956f245a7b96f49f5a
EwanEwart@HotMail.Co.UK <EwanEwart@HotMail.Co.UK>
2024-09-13 22:24:31 +0200

    update tf1_01.C

--- tf1_01.C
+++ tf1_01.C
@@ -2,21 +2,19 @@
 void tf1_01()
 {
    auto fa1 {
-	new TF1
-	(
-		  "fa1"			// char const * 	name
-		, "sin(x)/x" 	// char const * 	formula
-		, 0				// Double_t			xmin = 0
-		, 10				// Double_t			xmax = 1
- 							// EAddToList  	addToGlobList = EAddToList::kDefault,
-							// bool  			vectorize = false
-	)
-	};
+   new TF1
+   (
+        "fa1"        // char const *   name
+      , "sin(x)/x"   // char const *   formula
+      , 0            // Double_t       xmin = 0
+      , 10           // Double_t       xmax = 1
+                     // EAddToList     addToGlobList = EAddToList::kDefault
+                     // bool           vectorize = false
+   )
+   };
    fa1->Draw();
 }	
 /*
-
-
 F1 constructor using a formula definition.
 
 See TFormula constructor for explanation of the formula syntax.
@@ -25,6 +23,9 @@ See tutorials: fillrandom, first, fit1, formula1, multifit for real examples.
 
 Creates a function of type A or B between xmin and xmax
 
-if formula has the form "fffffff;xxxx;yyyy", it is assumed that the formula string is "fffffff" and "xxxx" and "yyyy" are the titles for the X and Y axis respectively.
+if formula has the form "fffffff;xxxx;yyyy",
+it is assumed that the formula string is
+"fffffff" and "xxxx" and "yyyy" are
+the titles for the X and Y axis respectively.
 
 */
