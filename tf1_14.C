4859c6d6b460080bcc9b771758b4e8cb327bb2f5
EwanEwart <EwanEwart@HotMail.Co.UK>
2024-09-24 20:14:30 +0200

    update tf1_14.C

--- tf1_14.C
+++ tf1_14.C
@@ -2,21 +2,26 @@
 
 void tf1_14(int choice=-1)
 {
-	// TF1 obj fcos
+	// TF1 obj "fcos"
 	auto fcos { new TF1 ("fcos", "[0] * cos(x)", 0., 10.) };
 	fcos->SetParNames( "cos");
 	fcos->SetParameter( 0, 1.1);
-	// TF1 obj fsin 
+
+	// TF1 obj "fsin" 
 	auto fsin { new TF1 ("fsin", "[0] * sin(x)", 0., 10.) };
 	fsin->SetParNames( "sin");
 	fsin->SetParameter( 0, 2.1);
-	// TF1 obj fCos_plus_fSin references TF1 objects fcos & fsin 
+
+	// TF1 obj fCos_plus_fSin references TF1 objects "fcos" & "fsin" 
 	auto fCos_plus_Sin { new TF1 ("fsc", "fcos + fsin") };
-	// TF1 object fSin_plus_Sin references TF1 object fsc 
+
+	// TF1 object fSin_plus_Sin references TF1 object "fsc" 
 	auto fSin_plus_Sin { new TF1 ("fs2", "fsc + fsc") };
+
 // That is, TF1 functions may be created as the predefined ones, e.g., cosine(x)
 	auto cosine { new TF1 ("cosine", "cos(x)",0.,10.) };
 	auto sine { new TF1 ( "sine", "sin(x)",0.,10.) };
+   auto cosine_plus_sine {new TF1("cps","cosine(x)+sine(x)",.0,10.) };
 	switch (choice) {
 		case 1: fcos->Draw(); break;
 		case 2: fsin->Draw(); break;
@@ -24,6 +29,7 @@ void tf1_14(int choice=-1)
 		case 4: fSin_plus_Sin->Draw(); break;
 		case 5: cosine->Draw();break;
 		case 6: sine->Draw();break;
+      case 7: cosine_plus_sine->Draw();break;
 		default:
 		cerr<<"unkown option"<<endl;
 	}
