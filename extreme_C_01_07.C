d893d34ebe3a8ba3eca500e1596d6f523dc513d3
EwanEwart <EwanEwart@HotMail.Co.UK>
2024-10-14 09:17:39 +0200

    update

---
+++ extreme_C_01_07.C
@@ -0,0 +1,15 @@
+#define PRINT(v) printf("%d ", v);
+#define PRINTLN printf("\n");
+#define RUN_LOOP(c, s, e)       \
+   for (c = s; c <= e; ++c) \
+   {                        \
+      PRINT(counter)        \
+   }                        \
+   PRINTLN
+
+int counter = 0;
+
+void extreme_C_01_07()
+{
+   RUN_LOOP(counter, 1, 10)
+}
