ea6aa1e6ecd957d220ac5712e61ba5406285f00d
EwanEwart <EwanEwart@HotMail.Co.UK>
2024-10-18 20:11:40 +0200

    update

--- app_03.cxx
+++ app_03.cxx
@@ -1,28 +1,28 @@
 /*
-Interactive example displaying a canvasPermalink
-================================================
+Interactive example displaying a canvas
+=======================================
 
 Use TApplication 
 to display the output on a screen. 
 
-TApplication creates a ROOT application environment 
-that provides an interface 
+*TApplication* creates a *ROOT application environment*
+that provides an *interface*
 - to the windowing system event loops and 
 - event handlers.
 
-To run the canvas as a standalone application 
-you must create a TApplication object. 
+To run the canvas as a *standalone application*
+you must create a *TApplication* object. 
 
 Calling the Run() method starts the event loop.
 
 */
 
-#include<TCanvas>
-#include<TF1.h>
-#include<TRootCanvas>
-#include <TApplication>
+#include <TApplication.h>
+#include <TCanvas.h>
+#include <TF1.h>
+#include <TRootCanvas.h>
 
-int main(int argc, char const *argv[])
+int main(int argc, char* argv[])
 {
    /*
    TApplication::TApplication 	
@@ -33,11 +33,33 @@ int main(int argc, char const *argv[])
 		Int_t  	    numOptions = 0 
 	) 	
    */
-   TApplication
+   TApplication app
    (
       "app"
-      ,&argc,argv);
+      ,&argc
+      ,argv
+   );
+   auto c { new TCanvas("c","Trig Fcnts",0+400,0+100,800/2+50,600/2+50) };
+   auto f1{ new TF1("f1","sin(x)",-5.,5.)};
+   f1->SetLineColor(kBlue+1);
+   f1->SetTitle("sin;x;sin(x)");
+   f1->Draw();
 
+   c->Modified();
+   c->Update();
+   auto rc = static_cast<TRootCanvas*>(c->GetCanvasImp());
+   rc->Connect
+   (
+       "CloseWindow()"
+      ,"TApplication"
+      ,gApplication
+      ,"Terminate()"
+   );
+
+   app.Run(); // start event loop
 
    return 0;
 }
+/*
+g++ app_03.cxx $(root-config --glibs --cflags --libs) -o app_03
+*/
---
+++ app_04.cxx
@@ -0,0 +1,48 @@
+/*
+
+https://root.cern/manual/creating_a_user_application/
+
+Example using ROOT prompt
+=========================
+
+You can use TRint to create an environment
+providing an interface
+- to the windows manager and
+- window's event loop
+via the inheritance of TApplication.
+
+In addition TRint provides interactive access
+to the Cling C++ interpreter
+via the command line.
+
+*/
+
+
+/* Rint is the (R)OOT (Int)eractive Interface.
+It allows interactive access
+to the ROOT system via a C++ interpreter. */
+#include <TRint.h>
+
+#include <TCanvas.h>
+#include <TF1.h>
+
+int main(int argc, char *argv[])
+{
+   TRint app("app", &argc, argv);
+   auto c{new TCanvas("c", "TrigFunct", 400, 100, 800 / 2 + 50, 600 / 2 + 50)};
+   auto f1{new TF1("f1", "sin(x)/x", -5., 5)};
+   f1->SetLineColor(kBlue+1);
+   f1->SetTitle("sin(x)/x;x;sin(x)/x");
+   f1->Draw();
+
+   c->Modified();
+   c->Update();
+
+   app.Run();
+
+   return 0;
+}
+
+/*
+g++ app_04.cxx $(root-config --glibs --cflags --libs) -o app_04
+*/
