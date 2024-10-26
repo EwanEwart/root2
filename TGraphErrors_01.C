b59095d71a27000b00c3c1a229d4e69146a224ec
EwanEwart <EwanEwart@HotMail.Co.UK>
2024-10-26 22:44:01 +0200

    update

--- TGraphErrors_01.C
+++ TGraphErrors_01.C
@@ -1,9 +1,18 @@
 void TGraphErrors_01()
 {
-   auto c {new TCanvas("c","Demo TGraphErrors",600,400)};
+   auto c {new TCanvas("c","Demo TGraphErrors",480,640)};
    auto g{new TGraph("ExampleData.txt", "%lg %lg")};
    auto gr{new TGraphErrors("ExampleData.txt")};
+
+   g->SetLineWidth(3);
+   g->SetLineColor(kBlue+1);
    g->Draw("AP");
+   
+   g->SetLineWidth(3);
+   g->SetLineColor(kGreen+1);
    g->Draw("C");
+   
+   gr->SetLineWidth(1);
+   gr->SetLineColor(kRed+2);
    gr->Draw("SAME");
 }
