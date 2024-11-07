d7774dc1ac5e9f98008ce36a61439aca4af74a18
EwanEwart <EwanEwart@HotMail.Co.UK>
2024-11-07 20:08:23 +0100

    update

--- TH1_02.C
+++ TH1_02.C
@@ -31,11 +31,7 @@ void put_into_bin(BINS &bins, double x, double from_x, double to_x)
       }
 }
 
-<<<<<<< HEAD:TH1F_04.C
-void TH1F_04()
-=======
 void TH1_02()
->>>>>>> 56eb2ce (update):TH1_02.C
 {
    auto title{"histogramme from file data"};
    auto nbinsx{10};
@@ -43,53 +39,6 @@ void TH1_02()
    auto xup{5.};
    BINS bins(nbinsx);
 
-<<<<<<< HEAD:TH1F_04.C
-   int datum{};
-
-   #define FILE_IN_1 "lotto.since.1955.csv"
-   #define FILE_IN_2 "lotto.since.1955.2.lines.csv"
-   #define FILE_OUT "lotto.since.1955.no.only.csv"
-   // auto fp_data_in = std::fopen(FILE_IN,"r");
-   auto fp_data_in = std::fopen(FILE_IN_2,"r");
-   auto fp_data_out = std::fopen(FILE_OUT,"w");
-   
-   if (fp_data_in)
-   {
-      std::cout<<FILE_IN_2<<" successfully."<<std::endl;
-   }
-   else
-   {
-      std::cerr<<"Failure : Could not open ."<<FILE_IN_2<<std::endl;
-      return;
-   }
-   
-   if (fp_data_out)
-   {
-      std::cout<<"Opened "<<FILE_OUT <<" successfully."<<std::endl;
-   }
-   else
-   {
-      std::cerr<<"Failure : Could not open file "<<FILE_OUT<<std::endl;
-      return;
-   }
-   
-   unsigned short field_no{};
-   for ( size_t filed_no{}; EOF != fscanf(fp_data_in,"%d",&datum); ++field_no)
-   {
-      auto const total_columns{11};
-      auto column{ (field_no%total_columns) };
-      if ( 2 < column && column < 9 )
-      {
-         put_into_bin(bins, datum, xlow, xup);
-         std::fprintf(fp_data_out,"%d\t",datum);
-         std::cout<<field_no%11<<'-';
-      }
-   }
-   std::cout<<std::endl;
-   
-   std::fclose(fp_data_in);
-   std::fclose(fp_data_out);
-=======
    std::ifstream data_fs{};
    decltype(xlow) datum{};
 
@@ -101,7 +50,6 @@ void TH1_02()
       h->Fill(datum);
       put_into_bin(bins, datum, xlow, xup);
    }
->>>>>>> 56eb2ce (update):TH1_02.C
 
    data_fs.close();
 
