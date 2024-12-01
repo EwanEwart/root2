9b5f99f4d975c926ba00a59bf76615a0f9c2069f
EwanEwart <EwanEwart@HotMail.Co.UK>
2024-12-01 19:44:24 +0100

    update

---
+++ thread_guard.C
@@ -0,0 +1,18 @@
+class thread_guard
+{
+   std::thread& t;
+public:
+   explicit thread_guard(thread& t_)
+   : t(t_)
+   {}
+   ~thread_guard()
+   {
+      if(t.joinable())
+      {
+         t.join();
+      }
+   }
+   thread_guard(thread_guard const&)=delete;
+   thread_guard& operator=(thread_guard const&)=delete;:w
+
+};
