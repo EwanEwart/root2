b2cd8f715b63b0ad9ba248002593b8346674b701
EwanEwart <EwanEwart@HotMail.Co.UK>
2024-12-10 11:22:30 +0100

    update

--- guess_the_number.C
+++ guess_the_number.C
@@ -11,22 +11,30 @@ int random_number(int min, int max)
    else
       return randGen(max, min);
 }
+void get_int(int &val, string const &prompt)
+{
+   cout << ">> " << prompt;
+   string str_val;
+   cin >> str_val;
+   if (str_val.empty())
+   {
+      cout << endl;
+      exit(-1);
+   }
+   else
+   {
+      val = atoi(str_val.c_str());
+   }
+}
 void guess_the_number()
 {
    cout << ">> Press ctrl-d to terminate the program." << endl;
 
-   // min
-   cout << ">> Enter min ... ";
-   string str_min;
-   cin >> str_min;
-   int min; int max;
-   if (str_min.empty()) { cout<<endl;return; } else { auto min{atoi(str_min.c_str())};}
+   int min;
+   get_int(min, "Enter min ... ");
 
-   // max
-   cout << ">> Enter max ... ";
-   string str_max;
-   cin >> str_max;
-   if (str_max.empty()) { cout<<endl;return; } else { auto max{atoi(str_max.c_str())};}
+   int max;
+   get_int(max, "Enter max ... ");
 
    auto carry_on{true};
    for (; carry_on;)
@@ -35,11 +43,8 @@ void guess_the_number()
       auto rn{random_number(min, max)};
       for (;;)
       {
-         string str_guess;
-         cout << ">> Guess the number: ... ";
-         cin >> str_guess;
-         if (str_guess.empty()) { cout << endl; carry_on = false; break; }
-         auto guess{atoi(str_guess.c_str())};
+         int guess;
+         get_int(guess, "Guess the number: ... ");
 
          cout << ">> Your guess: " << guess << endl;
          if (guess == rn)
@@ -51,13 +56,9 @@ void guess_the_number()
          else
          {
             if (rn < guess)
-            {
                cout << '(' << rn << ')' << " < " << guess << endl;
-            }
             else
-            {
                cout << guess << " < " << '(' << rn << ')' << endl;
-            }
          }
       }
    }
